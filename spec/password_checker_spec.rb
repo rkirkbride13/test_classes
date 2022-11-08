443f8bea15523d6468e4bc2a48a21cbccc7e6eab
Daisy <ysiad926@gmail.com>
2022-11-08 16:38:50 +0000

    password_checker third test

--- spec/password_checker_spec.rb
+++ spec/password_checker_spec.rb
@@ -13,4 +13,9 @@ RSpec.describe PasswordChecker do
       expect{password_checker.check("robbie")}.to raise_error "Invalid password, must be 8+ characters."
     end
   end
+
+  it "should return NoMethod erron when the password is integer" do
+    password_checker = PasswordChecker.new
+    expect{password_checker.check(123)}.to raise_error(NoMethodError)
+  end
 end

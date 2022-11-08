e54478b33d183ae34568e05ebcfa0578c50dba1c
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2022-11-08 15:16:35 +0000

    Gratitudes with second test

--- spec/gratitudes_spec.rb
+++ spec/gratitudes_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Gratitudes do
     result = gratitude.is_a?(Gratitudes)
     expect(result).to eq true
   end
+
+  it "should push a value into the gratitudes array if add method is called" do
+    gratitude = Gratitudes.new
+    result = gratitude.add("sunshine")
+    expect(result).to eq ["sunshine"]
+  end
+
 end
